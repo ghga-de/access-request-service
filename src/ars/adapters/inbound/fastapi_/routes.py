05c64800b79457d158fc0a294011deb4b4fb8b86
TheByronHimes <TheByronHimes@gmail.com>
2025-05-07 08:05:16 +0000

    Populate dataset_xyz fields in access request upon creation

--- src/ars/adapters/inbound/fastapi_/routes.py
+++ src/ars/adapters/inbound/fastapi_/routes.py
@@ -76,6 +76,8 @@ async def create_access_request(
         )
     except repository.AccessRequestAuthorizationError as exc:
         raise HTTPException(status_code=403, detail=str(exc)) from exc
+    except repository.DatasetNotFoundError as exc:
+        raise HTTPException(status_code=404, detail=str(exc)) from exc
     except repository.AccessRequestInvalidDuration as exc:
         raise HTTPException(status_code=422, detail=str(exc)) from exc
     except Exception as exc:
